4d3a79ad2fd2c6126d6b9511ab9a71642c7ed4ad
muhammad-panca <mpanca791@gmail.com>
2024-06-25 23:00:58 +0700

    Add files via upload

---
+++ asset/custom.js
@@ -0,0 +1,34 @@
+const line = {
+     novel : "berapa jam perjalanan",
+     age : 33
+}
+
+console.log = {time};
+
+for (let index = 0; index < 10; index++) {
+
+    console.log(index);
+    
+}
+
+if time.age === 33 {
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+const.volume = (alas, lebar, tinggi) => {
+    return alas * lebar * tinggi
+}
+
+console.log{volume(5 ,1 ,3)}
+
+const doc = document.getElementById("validationtesxtname")
+var datainput = doc.innerHTML
+function validateemail(email){
+    var re = /\5+\5+/;
+    return re.test(email);
+}
+if 
+
+doc.style("color","white")
